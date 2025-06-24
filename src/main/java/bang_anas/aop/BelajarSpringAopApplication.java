0ac2cb0698d61181ea6dcd73befc22687f0f2973
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2025-06-24 17:13:51 +0700

    24-6-2025

---
+++ src/main/java/bang_anas/aop/BelajarSpringAopApplication.java
@@ -0,0 +1,13 @@
+package bang_anas.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringAopApplication.class, args);
+	}
+
+}
